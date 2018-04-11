e8aa17e7b25a3941a0ad0d3dfa553a08f3bafc43
Joo <joo@joostory.net>
2018-04-11 22:29:41 +0900

    add option

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Make Sample Images
 
-sample 이미지를 찍어냅니다.
+sample용 이미지를 만듭니다.
+
+## 사용하기 전에
+
+이미지를 만들기 위해서 [Pillow](https://github.com/python-pillow/Pillow)가 필요합니다.
+
+```
+$ pip install Pillow
+```
+
+## usage
+
+```
+Usage: make-sample-image.py [options] count
+
+Options:
+  -h, --help       show this help message and exit
+  --prefix=PREFIX  filename prefix
+  --ext=EXT        file ext ('png', 'jpg', 'gif')
+  --output=OUTPUT  output dir
+  --width=WIDTH    image width
+  --height=HEIGHT  image height
+```
--- make-sample-image.py
+++ make-sample-image.py
@@ -4,53 +4,70 @@ from optparse import OptionParser
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-def makeImage(count, size, name):
-    print('make {} image ({}x{})'.format(count, size[0], size[1]))
+class ImageMaker:
+    def __init__(self, count, size, prefix, output, ext):
+        self.count = count
+        self.size = size
+        self.prefix = prefix
+        self.output = output
+        self.ext = ext
+    
+    def prepareOutputDir(self):
+        if not os.path.exists(self.output):
+            os.makedirs(self.output)
 
-    font = ImageFont.truetype(
-        font='RobotoSlab-Bold.ttf',
-        size=int(size[1]/3)
-    )
+    def getFilenameFormat(self):
+        prefix = '{}_'.format(self.prefix) if self.prefix else ''
+        return '{}/{}{{:03d}}.{}'.format(self.output, prefix, self.ext)
 
-    outputDir = 'output'
+    def make(self):
+        self.prepareOutputDir()
+        filenameFormat = self.getFilenameFormat()
 
-    if not os.path.exists(outputDir):
-        os.makedirs(outputDir)
-
-    for i in range(1, count + 1):
-        filename = '{}/{}_{:03d}.jpg'.format(outputDir, name, i)
-
-        print("make {}".format(filename))
-        text = str(i)
-        im = Image.new(mode = 'RGB', size = size, color=(255,255,255))
-        draw = ImageDraw.Draw(im)
-        w, h = draw.textsize(text, font)
-        draw.text(
-            xy=((size[0] - w) / 2, (size[1] - h) / 2),
-            text=text,
-            fill=(0,0,0),
-            font=font
+        width, height = self.size
+        font = ImageFont.truetype(
+            font = 'RobotoSlab-Bold.ttf',
+            size = int(height/3)
         )
-        im.save(filename)
+
+        for i in range(1, self.count + 1):
+            filename = filenameFormat.format(i)
+
+            print('make {}'.format(filename))
+            text = str(i)
+            im = Image.new(mode = 'RGB', size = self.size, color=(255,255,255))
+            draw = ImageDraw.Draw(im)
+            w, h = draw.textsize(text, font)
+            draw.text(
+                xy = ((width - w) / 2, (height - h) / 2),
+                text = text,
+                fill = (0,0,0),
+                font = font
+            )
+            im.save(filename)    
 
 
 def main():
-    usage = "usage: %prog [options] count"
+    usage = 'usage: %prog [options] count'
     parser = OptionParser(usage)
-    parser.add_option("--name", type="string", default="image")
-    parser.add_option("--width", type="int", default=800)
-    parser.add_option("--height", type="int", default=600)
+    parser.add_option('--prefix', type='string', help='filename prefix')
+    parser.add_option('--ext', type='string', default='png', help='file ext (\'png\', \'jpg\', \'gif\')')
+    parser.add_option('--output', type='string', default='output', help='output dir')
+    parser.add_option('--width', type='int', default=600, help='image width')
+    parser.add_option('--height', type='int', default=600, help='image height')
 
     (options, args) = parser.parse_args()
     if len(args) != 1:
-        parser.error("no count")
+        parser.error('no count')
 
-
-    makeImage(
-        count=int(args[0]),
+    imageMaker = ImageMaker(
+        count = int(args[0]),
         size = (options.width, options.height),
-        name=options.name
+        prefix = options.prefix,
+        output = options.output,
+        ext = options.ext
     )
+    imageMaker.make()
 
 
 if __name__ == '__main__':
